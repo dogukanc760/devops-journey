4a79aef8529ced31f5ae81fb5d094c486f528d4e
Dogukan Canerler <dogukanc760@hotmail.com>
2026-07-28 23:15:39 +0300

    test atlantis plan

---
+++ madde-2-iac/01-drift-detection/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+  }
+}
+
+resource "local_file" "drift_test" {
+  filename = "${path.module}/drift-test.txt"
+  content  = "Bu dosya Terraform tarafindan yonetiliyor. Versiyon: 1\n"
+}
+# yorum satırı ekle
